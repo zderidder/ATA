4b16b4bbfb0551cd6caf950a482caec223c1ab23
reithert2424 <reithert2424@uwec.edu>
2021-09-20 13:46:20 -0500

    Adding Python Script

---
+++ ata.py
@@ -0,0 +1,28 @@
+#Import Libraries
+
+#Display Launch Prompt
+
+#Retrieve List of Devices
+
+#Prompt User to Select Device
+
+#Prompt User to Specify Session Length
+
+#Prompt User for Application to launch
+
+#Start Tracking Session
+
+#Pipe log output to file. Show time remaining for session, prompt to end session early.
+
+#Load File
+
+#Process Data line-by-line using dictionary
+
+#Format Data in CSV Format.
+
+#Save Data to Drive
+
+#Upload to One-Drive Account.
+
+#Profit.
+
